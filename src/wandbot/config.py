58d64ff8e2f6b4a283365ca77c1f9535e0b53e81
Morgan McGuire <morganmcguire@Morgans-MacBook-Pro.local>
2023-03-29 16:45:08 -0400

    revert from 32k

--- src/wandbot/config.py
+++ src/wandbot/config.py
@@ -8,5 +8,5 @@ default_config = SimpleNamespace(
     faiss_artifact="parambharat/wandb_docs_bot/faiss_store:latest",
     hyde_prompt_artifact="parambharat/wandb_docs_bot/hyde_prompt:latest",
     chat_prompt_artifact="parambharat/wandb_docs_bot/system_prompt:latest",
-    model_name="gpt-4-32k",
+    model_name="gpt-4",
 )
