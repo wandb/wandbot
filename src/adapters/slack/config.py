10fd42ac8f9588d2b9ce23a170e8802c66347c8f
Anish Shah <anish@wandb.com>
2023-04-18 11:51:49 -0400

    Fix intro messages

--- src/adapters/slack/config.py
+++ src/adapters/slack/config.py
@@ -1,4 +1,4 @@
 
-INTRO_MESSAGE = f"""Please note that **wandbot is currently in alpha testing** and will experience frequent updates.\n\nPlease do not share any private or sensitive information in your query at this time.\n\nPlease note that overly long messages (>4097 tokens) will not be processed!\n\nGenerating response...\n\n"""
+INTRO_MESSAGE = f"""Please note that **wandbot is currently in alpha testing** and will experience frequent updates.\n\nPlease do not share any private or sensitive information in your query at this time.\n\nPlease note that overly long messages (>4097 tokens) will be truncated!\n\nGenerating response...\n\n"""
 
 OUTRO_MESSAGE = f"""🤖 If you still need help please try re-phrase your question, or alternatively reach out to the Weights & Biases Support Team at support@wandb.com \n\n Was this response helpful? Please react below to let us know"""
