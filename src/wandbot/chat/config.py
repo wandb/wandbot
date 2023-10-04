28539e49688fed3489488e10698b0b531e6142ab
Bharat Ramanathan <bharat.ramanathan@wandb.com>
2023-10-04 15:33:49 +0530

    feat: update chat temperature

--- src/wandbot/chat/config.py
+++ src/wandbot/chat/config.py
@@ -9,7 +9,7 @@ class ChatConfig(BaseSettings):
     max_retries: int = 2
     fallback_model_name: str = "gpt-3.5-turbo-16k-0613"
     max_fallback_retries: int = 6
-    chat_temperature: float = 0.0
+    chat_temperature: float = 0.1
     chat_prompt: pathlib.Path = pathlib.Path("data/prompts/chat_prompt.json")
     index_artifact: str = "wandbot/wandbot-dev/wandbot_index:latest"
     embeddings_cache: pathlib.Path = Field(pathlib.Path("data/cache/embeddings"), env="EMBEDDINGS_CACHE_PATH")
