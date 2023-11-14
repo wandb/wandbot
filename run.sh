aaa9ecc17e05eb9460573f35446f0da4234502fe
Artsiom <115654725+ArtsiomWB@users.noreply.github.com>
2023-11-14 19:51:58 +0000

    made the zd script match the same format as others

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 (uvicorn wandbot.api.app:app --host="0.0.0.0" --port=8000) & \
 (python -m wandbot.apps.slack) & \
 (python -m wandbot.apps.discord) & \
-(python src/wandbot/apps/ZD/ZDWandbot.py)
+(python -m wandbot.apps.ZD)
