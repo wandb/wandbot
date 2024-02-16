80029c5efe4d55155bfad67cc76bba3c57e5e650
Bharat Ramanathan <param.bharat@gmail.com>
2024-02-16 20:28:04 +0530

    chore: run formatters and linters

--- src/wandbot/rag/retrieval.py
+++ src/wandbot/rag/retrieval.py
@@ -3,6 +3,7 @@ from typing import List
 from langchain.retrievers.document_compressors import CohereRerank
 from langchain_core.documents import Document
 from langchain_core.runnables import Runnable, RunnablePassthrough
+
 from wandbot.ingestion.config import VectorStoreConfig
 from wandbot.rag.utils import get_web_contexts
 from wandbot.retriever.base import VectorStore
