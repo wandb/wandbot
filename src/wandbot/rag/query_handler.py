111b740d1eee8557bdf8f08ef13d6d966c297fab
Bharat Ramanathan <param.bharat@gmail.com>
2024-06-05 13:51:24 +0530

    chore: imports and formatting changes

--- src/wandbot/rag/query_handler.py
+++ src/wandbot/rag/query_handler.py
@@ -15,7 +15,6 @@ from langchain_core.runnables import (
 )
 from langchain_openai import ChatOpenAI
 from pydantic.v1 import BaseModel, Field
-
 from wandbot.rag.utils import ChatModel
 from wandbot.utils import get_logger
 
