a0bad63b6dd5d7a3fd5bc5e694f5e18a7e012340
olachinkei <keisuke.kamata@wandb.com>
2024-09-20 13:26:40 +0900

    add more configs to evaluation main

--- src/wandbot/evaluation/config.py
+++ src/wandbot/evaluation/config.py
@@ -3,6 +3,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class EvalConfig(BaseSettings):
+    evaluation_strategy_name: str = Field("ja v1.2.0-beta gpt3.5", description="Will be shown in evaluation page, and be used for just visibility")
+    eval_dataset: str = Field(
+        "weave:///wandbot/wandbot-eval-jp/object/wandbot_eval_data_jp:oCWifIAtEVCkSjushP0bOEc5GnhsMUYXURwQznBeKLA"
+        ,description="Dataset reference for evaluation"
+    )
+    # en evaluation dataset: "weave:///wandbot/wandbot-eval/object/wandbot_eval_data:eCQQ0GjM077wi4ykTWYhLPRpuGIaXbMwUGEB7IyHlFU"
+    # jp evaluation dataset: "weave:///wandbot/wandbot-eval-jp/object/wandbot_eval_data_jp:oCWifIAtEVCkSjushP0bOEc5GnhsMUYXURwQznBeKLA" 
+    # jp small evaluation dataset: "weave:///wandbot/wandbot-eval-jp/object/wandbot_eval_data_jp_test:Qp5qAlNYhzLJSfDZONaBNDzjhEmlfOTXNI1NvYhELKQ"
+
+    language: str = Field("ja", description="language for application (en or ja)")
+
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="allow"
     )
@@ -34,3 +45,5 @@ class EvalConfig(BaseSettings):
     )
     wandb_entity: str = Field("wandbot", env="WANDB_ENTITY")
     wandb_project: str = Field("wandbot-eval", env="WANDB_PROJECT")
+
+    
