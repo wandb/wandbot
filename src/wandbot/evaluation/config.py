37091a2d3c9a992b383763cf0ac1146d35da38f0
olachinkei <keisuke.kamata@wandb.com>
2024-10-21 21:45:07 +0900

    remove unnecessary artifact path

--- src/wandbot/evaluation/config.py
+++ src/wandbot/evaluation/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class EvalConfig(BaseSettings):
-    evaluation_strategy_name: str = Field("ja v1.2.0-beta gpt3.5", description="Will be shown in evaluation page, and be used for just visibility")
+    evaluation_strategy_name: str = Field("jp v1.2.0-beta", description="Will be shown in evaluation page, and be used for just visibility")
     eval_dataset: str = Field(
         "weave:///wandbot/wandbot-eval-jp/object/wandbot_eval_data_jp:oCWifIAtEVCkSjushP0bOEc5GnhsMUYXURwQznBeKLA"
         ,description="Dataset reference for evaluation"
@@ -14,36 +14,10 @@ class EvalConfig(BaseSettings):
 
     language: str = Field("ja", description="language for application (en or ja)")
 
-    model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", extra="allow"
-    )
-    eval_artifact: str = Field(
-        "wandbot/wandbot-eval/autoeval_dataset:v3",
-        env="EVAL_ARTIFACT",
-        validation_alias="eval_artifact",
-    )
-    eval_artifact_root: str = Field(
-        "data/eval",
-        env="EVAL_ARTIFACT_ROOT",
-        validation_alias="eval_artifact_root",
-    )
-
-    eval_annotations_file: str = Field(
-        "wandbot_cleaned_annotated_dataset_11-12-2023.jsonl",
-        env="EVAL_ANNOTATIONS_FILE",
-        validation_alias="eval_annotations_file",
-    )
-    eval_output_file: str = Field(
-        "eval.jsonl",
-        env="EVAL_OUTPUT_FILE",
-        validation_alias="eval_output_file",
-    )
     eval_judge_model: str = Field(
         "gpt-4-1106-preview",
         env="EVAL_JUDGE_MODEL",
         validation_alias="eval_judge_model",
     )
     wandb_entity: str = Field("wandbot", env="WANDB_ENTITY")
-    wandb_project: str = Field("wandbot-eval", env="WANDB_PROJECT")
-
-    
+    wandb_project: str = Field("wandbot-eval-jp", env="WANDB_PROJECT")
