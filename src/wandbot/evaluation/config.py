6e18524fd87d8e6780f77d446955f67163de36f3
olachinkei <keisuke.kamata@wandb.com>
2024-11-21 13:19:25 +0900

    add korean evaluation dataset

--- src/wandbot/evaluation/config.py
+++ src/wandbot/evaluation/config.py
@@ -3,12 +3,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class EvalConfig(BaseSettings):
-    evaluation_strategy_name: str = Field("jp v1.2.0-beta", description="Will be shown in evaluation page, and be used for just visibility")
+    evaluation_strategy_name: str = Field("en v1.2.0", description="Will be shown in evaluation page, and be used for just visibility")
     eval_dataset: str = Field(
         "weave:///wandbot/wandbot-eval-jp/object/wandbot_eval_data_jp:oCWifIAtEVCkSjushP0bOEc5GnhsMUYXURwQznBeKLA"
         ,description="Dataset reference for evaluation"
     )
-    language: str = Field("ja", description="language for application (en or ja)")
+    language: str = Field("en", description="language for application (en or ja or kr)")
 
     eval_judge_model: str = Field(
         "gpt-4-1106-preview",
@@ -16,4 +16,10 @@ class EvalConfig(BaseSettings):
         validation_alias="eval_judge_model",
     )
     wandb_entity: str = Field("wandbot", env="WANDB_ENTITY")
-    wandb_project: str = Field("wandbot-eval", env="WANDB_PROJECT")
+    wandb_project: str = Field("wandbot-eval-kr", env="WANDB_PROJECT")
+
+    #en: weave:///wandbot/wandbot-eval-jp/object/wandbot_eval_data_jp:oCWifIAtEVCkSjushP0bOEc5GnhsMUYXURwQznBeKLA
+    #jp: weave:///wandbot/wandbot-eval-jp/object/wandbot_eval_data_jp:oCWifIAtEVCkSjushP0bOEc5GnhsMUYXURwQznBeKLA
+    #kr: weave:///wandbot/wandbot-eval-kr/object/wandbot_eval_data_kr:9vnf9CU5V8eKkmHoiACyB01oQwtY3RmIo0XGH2cVhvo
+
+    
