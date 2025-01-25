9781cfa17d93ee1cc92160cbf62baefed84d269f
Morgan McGuire <morganmcg1@users.noreply.github.com>
2025-01-25 08:54:43 +0000

    modify eval message

--- src/wandbot/evaluation/eval.py
+++ src/wandbot/evaluation/eval.py
@@ -185,7 +185,7 @@ class WandbotCorrectnessScorer(weave.Scorer):
 
         if self.debug:
             if model_output is not None:
-                logger.info(f"In get_answer_correctness, model_output keys:\n{model_output.keys()}")
+                logger.info(f"In WandbotCorrectnessScorer, model_output keys:\n{model_output.keys()}")
             else:
                 logger.error("model_output is None")
     
