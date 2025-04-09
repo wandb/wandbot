3c8fdfe24d4d2551c42d93f8d922400a995520e3
Morgan McGuire <morganmcg1@users.noreply.github.com>
2025-04-09 15:28:50 +0100

    Comment out retriever route

--- src/wandbot/api/routers/retrieve.py
+++ src/wandbot/api/routers/retrieve.py
@@ -1,71 +1,71 @@
-from typing import Any, Dict, List
+# from typing import Any, Dict, List
 
-from fastapi import APIRouter
-from pydantic import BaseModel
-from starlette import status
+# from fastapi import APIRouter
+# from pydantic import BaseModel
+# from starlette import status
 
-from wandbot.retriever.base import SimpleRetrievalEngine
+# from wandbot.retriever.base import SimpleRetrievalEngine
 
-router = APIRouter(
-    prefix="/retrieve",
-    tags=["retrievers"],
-)
+# router = APIRouter(
+#     prefix="/retrieve",
+#     tags=["retrievers"],
+# )
 
-retriever: SimpleRetrievalEngine | None = None
-
-
-class APIRetrievalResult(BaseModel):
-    text: str
-    score: float
-    metadata: Dict[str, Any]
+# retriever: SimpleRetrievalEngine | None = None
 
 
-class APIRetrievalResponse(BaseModel):
-    query: str
-    top_k: List[APIRetrievalResult]
+# class APIRetrievalResult(BaseModel):
+#     text: str
+#     score: float
+#     metadata: Dict[str, Any]
 
 
-class APIRetrievalRequest(BaseModel):
-    query: str
-    language: str = "en"
-    top_k: int = 5
-    sources: List[str] | None = None
+# class APIRetrievalResponse(BaseModel):
+#     query: str
+#     top_k: List[APIRetrievalResult]
 
 
-@router.post(
-    "",
-    response_model=APIRetrievalResponse,
-    status_code=status.HTTP_200_OK,
-)
-@router.post(
-    "/",
-    response_model=APIRetrievalResponse,
-    status_code=status.HTTP_200_OK,
-)
-def retrieve(request: APIRetrievalRequest) -> APIRetrievalResponse:
-    """Retrieves the top k results for a given query.
+# class APIRetrievalRequest(BaseModel):
+#     query: str
+#     language: str = "en"
+#     top_k: int = 5
+#     sources: List[str] | None = None
 
-    Args:
-        request: The APIRetrievalRequest object containing the query and other parameters.
 
-    Returns:
-        The APIRetrievalResponse object containing the query and top k results.
-    """
-    results = retriever(
-        question=request.query,
-        language=request.language,
-        top_k=request.top_k,
-        sources=request.sources,
-    )
+# @router.post(
+#     "",
+#     response_model=APIRetrievalResponse,
+#     status_code=status.HTTP_200_OK,
+# )
+# @router.post(
+#     "/",
+#     response_model=APIRetrievalResponse,
+#     status_code=status.HTTP_200_OK,
+# )
+# def retrieve(request: APIRetrievalRequest) -> APIRetrievalResponse:
+#     """Retrieves the top k results for a given query.
 
-    return APIRetrievalResponse(
-        query=request.query,
-        top_k=[
-            APIRetrievalResult(
-                text=result["text"],
-                score=result["score"],
-                metadata=result["metadata"],
-            )
-            for result in results
-        ],
-    )
+#     Args:
+#         request: The APIRetrievalRequest object containing the query and other parameters.
+
+#     Returns:
+#         The APIRetrievalResponse object containing the query and top k results.
+#     """
+#     results = retriever(
+#         question=request.query,
+#         language=request.language,
+#         top_k=request.top_k,
+#         sources=request.sources,
+#     )
+
+#     return APIRetrievalResponse(
+#         query=request.query,
+#         top_k=[
+#             APIRetrievalResult(
+#                 text=result["text"],
+#                 score=result["score"],
+#                 metadata=result["metadata"],
+#             )
+#             for result in results
+#         ],
+#     )
