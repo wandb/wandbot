326c48e945412a2aa021e1e40da5412c287c0e19
Morgan McGuire <morganmcg1@users.noreply.github.com>
2025-06-22 21:41:32 +0100

    v56 index

--- src/wandbot/configs/vector_store_config.py
+++ src/wandbot/configs/vector_store_config.py
@@ -16,11 +16,11 @@ class VectorStoreConfig(BaseSettings):
     )
     
     # Vector Store
-    vectordb_collection_name: str = "chroma_index-v55" #"vectorstore", vectorstore-chroma_index-v54
+    vectordb_collection_name: str = "chroma_index-v56" #"vectorstore", vectorstore-chroma_index-v54
     vectordb_index_dir: pathlib.Path = Field(
         pathlib.Path("artifacts/vector_stores"), env="VECTORDB_INDEX_DIR"
     )
-    vectordb_index_artifact_url: str = "wandbot/wandbot-dev/chroma_index:v55"
+    vectordb_index_artifact_url: str = "wandbot/wandbot-dev/chroma_index:v56"
     distance: str = "l2"  # used in retrieval from vectordb 
     distance_key: str = "hnsw:space"  # used in retrieval from vectordb 
     
